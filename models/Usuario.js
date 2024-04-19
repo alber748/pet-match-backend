062fe0e130975270046e09c3f2b53f5d8e79c458
JESUS ALBERTO FUNES GARCIA <jesus.funes21@itca.edu.sv>
2024-04-19 00:37:33 -0600

    init commit

---
+++ models/Usuario.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+
+const UsuarioSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    kindRol: {
+        type: String,
+        required: true,
+    },
+    entidad: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = model('Usuario', UsuarioSchema );
